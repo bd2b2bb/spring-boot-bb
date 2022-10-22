29fc7c64345abe5bf3c16ece52ddbe418814f36c
bd2b2bb <mmnmm@me.com>
2022-10-22 14:24:19 +0900

    first commit

---
+++ src/main/java/com/bbb/springbootbb/SpringBootBbApplication.java
@@ -0,0 +1,13 @@
+package com.bbb.springbootbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bbb/springbootbb/SpringBootBbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbb.springbootbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
